53fab8144947790c125785d255727d0ca83fa313
vita500 <162125339+vita500@users.noreply.github.com>
2024-10-25 02:31:23 +0900

    웹페이지 제작 과제

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+document.querySelectorAll('#menu-list a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
